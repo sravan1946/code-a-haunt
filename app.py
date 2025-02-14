2e75e55c237fba91d50a94ce907df90064cec079
sravan1946 <miniclip1956@gmail.com>
2025-02-15 04:02:12 +0530

    set host

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True, port=8000)
+    app.run(debug=True, use_reloader=True, port=8000, host="0.0.0.0")
 
